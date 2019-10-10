3ef3a98a48ecc16f7e03e89ececa32e86bd25089
Lena Kosinski <lena-kosinski@uiowa.edu>
2019-10-10 18:52:43 -0500

    modified app to use basic Jinja templates

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello world"
+    return render_template('index.html', display = "", pageTitle = 'My Calculator')
 
 @app.route('/lena')
 def lena():
-    return "hello lena"
+    return "heyyyy"
 
 if __name__ == '__main__':
     app.run(debug=True)
