050dfa8409f90e251d18cb0c5a4e821647cde8f8
Lena Kosinski <lena-kosinski@uiowa.edu>
2019-10-10 19:59:40 -0500

    added calculation functionality to index and added style

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from flask import Flask
-from flask import render_template
-
+from flask import render_template, request, redirect
+from decimal import Decimal
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', display = "", pageTitle = 'My Calculator')
+    return render_template('index.html', display="", pageTitle = 'My Loan Calculator')
 
-@app.route('/lena')
-def lena():
-    return "heyyyy"
+@app.route('/calculate', methods=['GET', 'POST'])
+def calculate():
+    if request.method == 'POST':
+        form = request.form
+        loan_amount = int(form['loan_amount'])
+        number_payments = int(form['number_payments'])
+        interest_rate = Decimal(form['interest_rate'])
+        firstD = ((1+interest_rate)**number_payments)-1
+        secondD = (((1+interest_rate)**number_payments)*interest_rate)
+        calculate = '${:,.2f}'.format(Decimal(loan_amount/(firstD/secondD)))
+        return render_template('index.html', display=calculate, pageTitle="My Loan Calculator")
+
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
